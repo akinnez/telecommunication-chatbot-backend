8447372aebf724e733d4e61d019d2bf7d4926993
akinnez <oyetunjiidris@gmail.com>
2025-04-05 14:02:00 +0100

    implement cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://telecommunication-chatbot-frontend.vercel.app',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env['PORT'] || 10000);
 }
 bootstrap();
