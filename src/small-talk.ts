ee4bae4729339c364273908840bd365e1db2f6d0
akinnez <oyetunjiidris@gmail.com>
2025-04-12 10:26:26 +0100

    small talk improvement

--- src/small-talk.ts
+++ src/small-talk.ts
@@ -1,10 +1,12 @@
 export const SMALL_TALK_QA = [
   { question: 'hello', answer: 'Hi there! 👋 How can I help you today?' },
   { question: 'hi', answer: 'Hey! 😊 What can I do for you?' },
-  { question: 'how are you', answer: "I'm doing great! How about you?" },
+  { question: 'hi', answer: 'Hey! 😊 What can I do for you?' },
+  { question: 'Hey there!', answer: "I'm doing great! How about you?" },
+  { question: 'Yo!', answer: 'Hello! How can I help today?' },
   {
-    question: 'how’s it going',
-    answer: 'All systems go! 🚀 What can I do for you?',
+    question: 'how is it going',
+    answer: 'its going smooth! 🚀 What can I do for you?',
   },
   { question: "what's up", answer: 'Not much, just waiting to help you. 😊' },
   {
@@ -14,5 +16,55 @@ export const SMALL_TALK_QA = [
   {
     question: 'good afternoon',
     answer: 'Good afternoon!, How can I help today? 😎',
+  },
+  {
+    question: 'good evening',
+    answer: 'Good evening!, How can I help today? 😎',
+  },
+  {
+    question: 'I need help with something',
+    answer: 'What can I do to help you today? 🤔',
+  },
+  {
+    question: 'Can I ask you something?',
+    answer: 'Sure you can. How can I help you today? 🤔',
+  },
+  {
+    question: 'I have a question',
+    answer: 'How can I help you? 🤔',
+  },
+  {
+    question: 'Quick question for you',
+    answer: 'How can I help you? 🤔',
+  },
+  {
+    question: 'Do you have a second?',
+    answer: 'I have all the time in the world for you. How can I help you? 🤔',
+  },
+  {
+    question: 'Are you there?',
+    answer: 'Yes, I am here. What do you need? 🤓',
+  },
+  {
+    question: 'I am having a problem',
+    answer: 'What can I do for you?',
+  },
+  {
+    question: 'Can you assist me?',
+    answer: 'What can I do for you?',
+  },
+  {
+    question: 'Is this customer support?',
+    answer: 'Yes, it is. What can i do for you today?',
+  },
+  {
+    question: 'What services do you offer?',
+    answer:
+      'I offers wide range of telecommunication related services. Services which includes voice communication (landline and mobile phones), data and internet services (broadband, fiber optics, wireless internet), network infrastructure (tower installations, routing systems), and media and entertainment (cable TV, video streaming). Would you like detailed breakdown on telecommunication services?',
+  },
+  {
+    question: 'What products do you offer?',
+    answer:
+      'I offers wide range of telecommunication related products. Products which includes phones, modems, and routers, as well as services like broadband internet, mobile data, and VoIP. Would you like detail breakdown on telecommunication products?',
   },
 ];
